7eabe95e12b8c15a613056cfa1bb1c3910abeffd
TranTrungTinLV <trungtintin1989@gmail.com>
2024-05-01 19:17:02 +0700

    update Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,34 @@
 import { router } from 'expo-router'
 import React, { type ReactElement } from 'react'
-import { View } from 'react-native'
+import { Dimensions, View } from 'react-native'
 
 import { Button } from '../components/atoms/Button'
 import { Text } from '../components/atoms/Text'
 
+const { width } = Dimensions.get('screen')
 export default function Page (): ReactElement {
   return (
     <View style={{ gap: 16 }}>
-      <Text>This is Home page</Text>
+      <Text style={{ textAlign: 'center' }}>This is Home page</Text>
 
       <Button
         text="Go to About page"
         onPress={() => {
           router.push('/about')
+        }}
+      />
+
+      <Button
+        text="Plain"
+        onPress={() => {}}
+        style={{
+          paddingTop: 18,
+          paddingBottom: 18,
+          borderWidth: 1,
+          borderRadius: 9,
+          borderColor: '#0000',
+          backgroundColor: '#e1f0ff',
+          width: width * 0.4
         }}
       />
     </View>
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,17 +1,21 @@
 import React, { type ReactElement } from 'react'
-import { Pressable } from 'react-native'
+import { Pressable, type StyleProp, type ViewStyle } from 'react-native'
+import { type PressableProps } from 'react-native/Libraries/Components/Pressable/Pressable'
 
 import { Text } from '../Text'
 
-interface ButtonProps {
+type ButtonProps = PressableProps & {
   text: string
-  onPress: () => void
+  style?: StyleProp<ViewStyle>
 }
 
 export const Button = (props: ButtonProps): ReactElement => {
   return (
-    <Pressable onPress={props.onPress}>
-      <Text>{props.text}</Text>
+    <Pressable onPress={props.onPress} style={[props.style]}>
+      <Text style={{
+        textAlign: 'center',
+        color: '#00254d'
+      }}>{props.text}</Text>
     </Pressable>
   )
 }
